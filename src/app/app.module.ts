1a48083401ed9adfb2f6f815fa7e3fc12010504b
Diego Moreno <diego_maky@hotmail.com>
2024-03-05 22:59:52 +0100

    configuracion app

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
